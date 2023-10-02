8272c1d17427ce78d2d8bdaacf017359945dea89
qtambone <quintinobello91@gmail.com>
2023-10-02 12:23:36 +0200

    titre formulaire

---
+++ exo2-quentin-hugo/src/app/service/form-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormDataService { 
+  private formData: User | null = null; 
+
+  setFormData(data: User) {
+    this.formData = data;
+  }
+
+  getLastFormData() {
+    return this.formData ;
+  }
+}
